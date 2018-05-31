71cdc19ea1eda3c4f9a27baf08b5e1658da8c580
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-05-31 21:04:29 +0200

    to get all the numbers from with in a string
    output : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']

---
+++ Intermediate/get_all_the_numbers_in_a_string.py
@@ -0,0 +1,9 @@
+import re # import regular expressions
+
+str = "get 1 all 2 the 3 numbers 4 in 5 a 6 string 7 as 8 a 9 list 10" # string to be tested
+
+nums = list() # empty list
+
+nums = re.findall('[0-9]+', str) # look for the numbers in the string
+
+print(nums) # print the numbers
