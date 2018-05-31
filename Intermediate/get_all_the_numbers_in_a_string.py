11ac9b7b1c2ef6ae2f61210f7bf69f6f5492c962
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-05-31 21:08:06 +0200

    Update get_all_the_numbers_in_a_string.py
    added a comment

--- Intermediate/get_all_the_numbers_in_a_string.py
+++ Intermediate/get_all_the_numbers_in_a_string.py
@@ -1,3 +1,5 @@
+# this program can fetch all the numbers with in a string and create a list of all those numbers
+
 import re # import regular expressions
 
 str = "get 1 all 2 the 3 numbers 4 in 5 a 6 string 7 as 8 a 9 list 10" # string to be tested
