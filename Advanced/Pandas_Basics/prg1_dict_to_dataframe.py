46bc4487b54382849d0b53a458611eaa0d5b4fa6
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-11-16 19:16:39 +0100

    dictionary to data frame using pandas library

---
+++ Advanced/Pandas_Basics/prg1_dict_to_dataframe.py
@@ -0,0 +1,16 @@
+import pandas as pd # import the pandas module 
+
+# create a simple dictionary holding some key value pairs
+my_dict = {
+          "col1":[1,2,3,4],
+          "col2":[5,6,7,8],
+          "col3":[9,10,11,12],
+          "col4":[13,14,15,16]
+          }
+
+print(my_dict) # view the dictionary
+
+#convert dictionary into a dataframe
+dataframe = pd.DataFrame(my_dict) # use pandas inbuilt DataFrame method
+
+print(dataframe) # view the created dataframe
